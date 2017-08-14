6b20dc6cf258d69991d6aab9525bbc4dbd8b13d7
Rahul <rsembedded@gmail.com>
2017-08-14 19:14:26 +0530

    Python refresher

---
+++ Natural Language Basics/Basic Python.py
@@ -0,0 +1,22 @@
+# List
+# List store comma separated value in array fashion.It can store number, string or mix.
+my_list =['a','b','c']
+my_list2 =[1,2,3,4,5]
+my_list3 =[1,2,'a','b']
+print(my_list)
+print(my_list2)
+
+
+# Using Index use the value of the list. Index genrally started fron 0.
+
+print(my_list[0])
+print(my_list[:])
+print(my_list[1:])
+print(my_list[:-1])
+print(my_list[:-2])
+
+# print last 2 value
+print(my_list2[-2:])
+
+#revarce string
+print(my_list3[::-1])
